393db513ea0f37a06568723396da9ada41f95714
Andreas Ganske <a.ganske@chaosspace.de>
2022-10-02 21:53:23 +0200

    Bump to swift-tools-version 5.7

--- Package.swift
+++ Package.swift
@@ -1,10 +1,10 @@
-// swift-tools-version:5.0
+// swift-tools-version: 5.7
 import PackageDescription
 
 let package = Package(
     name: "ConvAPI",
     platforms: [
-        .iOS(.v10),
+        .iOS(.v11),
     ],
     products: [
         .library(name: "ConvAPI", targets: ["ConvAPI"]),
@@ -14,7 +14,10 @@ let package = Package(
         .package(url: "https://github.com/PromiseKit/Foundation.git", from: "3.0.0"),
     ],
     targets: [
-        .target(name: "ConvAPI", dependencies: ["PromiseKit", "PMKFoundation"]),
+        .target(name: "ConvAPI", dependencies: [
+            .product(name: "PromiseKit", package: "PromiseKit"),
+            .product(name: "PMKFoundation", package: "Foundation"),
+        ]),
         .testTarget(name: "ConvAPITests", dependencies: ["ConvAPI"]),
     ]
 )
